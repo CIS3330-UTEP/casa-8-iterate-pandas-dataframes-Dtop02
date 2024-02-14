040cabec24c8c1ce542b095d5bcd0bf9d4fa7a83
Dtop02 <156946626+Dtop02@users.noreply.github.com>
2024-02-14 10:37:22 -0700

    Update casa_8.py

--- casa_8.py
+++ casa_8.py
@@ -6,8 +6,8 @@ df = pd.read_csv('big-mac-full-index.csv')
 # # Iterate through the DataFrame rows using iterrows()
 for i,r in df.iterrows():
 #     # Access each row's data using row[column_name]
-    # print(r['date'], r['name'], r['USD_adjusted'])
+    print(r['name'], r['local_price'])
 
 #6  Using apply()
-    print(df.apply(lambda r: (r['date'], r['name'], r['USD_adjusted']), axis = 1))
+    print(df.apply(lambda r: r['name'], axis = 1))
 
