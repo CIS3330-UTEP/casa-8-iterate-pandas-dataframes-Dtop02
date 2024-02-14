f1948c7a86f7f94a02d470b7fd710ba0a028f1d0
Dtop02 <156946626+Dtop02@users.noreply.github.com>
2024-02-13 23:22:10 -0700

    There is a continuously loop running

--- casa_8.py
+++ casa_8.py
@@ -1,1 +1,13 @@
+import pandas as pd
+
+# # Read the CSV file into a DataFrame
+df = pd.read_csv('big-mac-full-index.csv')
+
+# # Iterate through the DataFrame rows using iterrows()
+for i,r in df.iterrows():
+#     # Access each row's data using row[column_name]
+    # print(r['date'], r['name'], r['USD_adjusted'])
+
+#6  Using apply()
+    print(df.apply(lambda r: (r['date'], r['name'], r['USD_adjusted']), axis = 1))
 
